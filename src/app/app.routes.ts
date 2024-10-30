b04f74b877e05f699dda8577e331112076bd83a0
Maksym Shyiko <maxymshyjko@gmail.com>
2024-10-30 21:28:05 +0000

    Layout setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,59 @@
 import { Routes } from '@angular/router';
+import path from 'node:path';
+import { LoginComponent } from './features/login/login.component';
+import { RegisterComponent } from './features/register/register.component';
+import { AccountComponent } from './features/account/account.component';
+import { HomeComponent } from './features/home/home.component';
+import { AuctionListComponent } from './features/auction-list/auction-list.component';
+import { AuctionComponent } from './features/auction/auction.component';
+import { LotListComponent } from './features/lot-list/lot-list.component';
+import { LotComponent } from './features/lot/lot.component';
+import { AccountListComponent } from './features/account-list/account-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'accounts',
+    component: AccountListComponent,
+  },
+  {
+    path: 'accounts/:id',
+    component: AccountComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'auctions',
+    component: AuctionListComponent,
+  },
+  {
+    path: 'auctions/:id',
+    component: AuctionComponent,
+  },
+  {
+    path: 'lots',
+    component: LotListComponent,
+  },
+  {
+    path: 'lots/:id',
+    component: LotComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  }
+];
---
+++ src/app/features/auction/auction.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auction',
+  standalone: true,
+  imports: [],
+  templateUrl: './auction.component.html',
+  styleUrl: './auction.component.scss'
+})
+export class AuctionComponent {
+
+}
