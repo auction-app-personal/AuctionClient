45daa788887723d8a51f4ca870b9f184d1331d5e
Maksym Shyiko <maxymshyjko@gmail.com>
2024-11-12 23:52:13 +0000

    Created auction page layout

--- src/app/features/auction/auction.component.ts
+++ src/app/features/auction/auction.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuctionDto } from '../../models/auction/auction.model';
+import { MockAuctionService } from '../../services/mock-data/mock-auction.service';
+import { ActivatedRoute } from '@angular/router';
+import { LotDto } from '../../models/lot/lot.model';
+import { MockLotService } from '../../services/mock-data/mock-lot.service';
+import { log } from 'console';
+import { LotPreviewComponent } from './lot-preview/lot-preview.component';
 
 @Component({
   selector: 'app-auction',
   standalone: true,
-  imports: [],
+  imports: [LotPreviewComponent],
   templateUrl: './auction.component.html',
-  styleUrl: './auction.component.scss'
+  styleUrl: './auction.component.scss',
 })
-export class AuctionComponent {
+export class AuctionComponent implements OnInit {
+  private projectId: number;
+  auction: AuctionDto | null = null;
+  lots: LotDto[] | null = null;
 
+  constructor(
+    private lotService: MockLotService,
+    private auctionService: MockAuctionService,
+    private route: ActivatedRoute
+  ) {
+    this.projectId = 0;
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      const idVal = params.get('id');
+      if (idVal) {
+        this.projectId = parseInt(idVal);
+      }
+    });
+    this.auction = this.auctionService.auctions[this.projectId];
+    this.lotService
+      .getLotsByProjectId(this.projectId)
+      .subscribe((data) => (this.lots = data));
+    console.log(this.lots);
+  }
 }
