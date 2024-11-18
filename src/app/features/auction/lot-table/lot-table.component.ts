923ef40324dd446765f3dd0eeb49c61ee0998192
Maksym Shyiko <maxymshyjko@gmail.com>
2024-11-18 21:48:13 +0000

    Created lot table component folder

---
+++ src/app/features/auction/lot-table/lot-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lot-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './lot-table.component.html',
+  styleUrl: './lot-table.component.scss'
+})
+export class LotTableComponent {
+
+}
