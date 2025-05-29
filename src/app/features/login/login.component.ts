842b6d2df60d2f703101bb6d67582d316d8f4924
M-Bell <maxymshyjko@gmail.com>
2025-05-29 22:26:46 +0100

    Created login form validation

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,16 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { log } from 'console';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {
-  submit() {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  loginField!: FormControl;
+  passwordField!: FormControl;
+
+  constructor() {}
+
+  onSubmit() {
     console.log('Submitted');
+    console.log(this.loginForm);
+    console.log(this.loginField);
+    console.log(this.passwordField);
+  }
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      login: new FormControl(this.loginField, [Validators.required]),
+      password: new FormControl(this.passwordField, [Validators.required]),
+    });
   }
 }
