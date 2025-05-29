d9acbfa12ee6fc7d16424d4e24bd9ecddcd5fa16
Maksym Shyiko <maksym.shyiko@booksoft.com.ua>
2025-05-29 22:11:04 +0100

    Created auction page layout

--- src/app/features/account/account.component.ts
+++ src/app/features/account/account.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { LotService } from '../../services/lot/lot-service.interface';
+import { AccountService } from '../../services/account/account-service.interface';
+import { ACCOUNT_SERVICE } from '../../services/common/injection-tokens';
+import { AccountDto } from '../../models/account/account.model';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-account',
@@ -7,6 +13,31 @@ import { Component } from '@angular/core';
   templateUrl: './account.component.html',
   styleUrl: './account.component.scss'
 })
-export class AccountComponent {
+export class AccountComponent implements OnInit, OnDestroy{
+  account: AccountDto | null = null;
+  private subscriptions: Subscription = new Subscription();
+
+
+  constructor(
+    @Inject(ACCOUNT_SERVICE) private accountService: AccountService,
+    private route: ActivatedRoute
+  )  {
+    
+  }
+  ngOnInit(): void {
+    const routeSubscription = this.route.paramMap.subscribe((params) => {
+      const idVal = params.get('id');
+      if (idVal) {
+        this.accountService.getById(parseInt(idVal)).subscribe(
+          account => this.account = account
+        )
+      }
+    });
+    this.subscriptions.add(routeSubscription);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 
 }
