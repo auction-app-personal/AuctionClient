0d3cea7d7da83bbd20ecfe9fc06bab17ab5ed03d
M-Bell <maxymshyjko@gmail.com>
2025-06-04 21:38:09 +0100

    Created lot page base layout

--- src/app/features/lot/lot.component.ts
+++ src/app/features/lot/lot.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Input, OnDestroy, OnInit } from '@angular/core';
+import { LotDto } from '../../models/lot/lot.model';
+import { LOT_SERVICE } from '../../services/common/injection-tokens';
+import { LotService } from '../../services/lot/lot-service.interface';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-lot',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './lot.component.html',
   styleUrl: './lot.component.scss'
 })
-export class LotComponent {
+export class LotComponent implements OnInit, OnDestroy{
+
+  lot: LotDto | null = null;
+  private subscriptions: Subscription = new Subscription();
+
+  constructor(
+    @Inject(LOT_SERVICE) private lotService: LotService,
+    private route: ActivatedRoute
+  )  {
+    
+  }
+
+  ngOnInit(): void {
+    const routeSub = this.route.paramMap.subscribe(
+      params => {
+        const idVal = params.get('id');
+        if(idVal){
+          this.lotService.getById(+idVal).subscribe(
+            lot => this.lot = lot
+          )
+        }
+      }
+    );
+    this.subscriptions.add(routeSub);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 
 }
