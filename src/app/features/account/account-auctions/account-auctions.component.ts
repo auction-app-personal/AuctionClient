384d25cb702bd8210ee430face22670166e558d7
M-Bell <maxymshyjko@gmail.com>
2025-06-19 22:44:06 +0100

    Added auction modal form opening functions

--- src/app/features/account/account-auctions/account-auctions.component.ts
+++ src/app/features/account/account-auctions/account-auctions.component.ts
@@ -1,26 +1,27 @@
 import { Component, Inject, Input, OnDestroy, OnInit, signal } from '@angular/core';
 import { AuctionDto } from '../../../models/auction/auction.model';
 import { Subscription } from 'rxjs';
-import { AUCTION_FACADE, AUCTION_SERVICE } from '../../../services/common/injection-tokens';
-import { AuctionService } from '../../../services/data/auction/auction-service.interface';
+import { AUCTION_FACADE } from '../../../services/common/injection-tokens';
 import { AuctionFacadeService } from '../../../services/shared/auction-facade.service';
+import { AuctionFormModalComponent } from "../../auction/auction-form-modal/auction-form-modal.component";
 
 @Component({
   selector: 'app-account-auctions',
   standalone: true,
-  imports: [],
+  imports: [AuctionFormModalComponent],
   templateUrl: './account-auctions.component.html',
   styleUrl: './account-auctions.component.scss'
 })
 export class AccountAuctionsComponent implements OnInit, OnDestroy{
+
+  private subscriptions: Subscription = new Subscription();
 
   @Input({ required: true })
   accountId: number = 0;
 
   ownedAuctions = signal<AuctionDto[]>([]);
   participatedAuctions = signal<AuctionDto[]>([]);
-
-  private subscriptions: Subscription = new Subscription();
+  isModalVisible = false;
 
   constructor(@Inject(AUCTION_FACADE) private auctionService: AuctionFacadeService){
     
@@ -49,6 +50,20 @@ export class AccountAuctionsComponent implements OnInit, OnDestroy{
         }
       }
     );
+  }
+
+  openAuctionModal(): void {
+    document.body.classList.add('modal-open');
+	  this.isModalVisible = true;
+  }
+
+  hideAuctionModal() {
+    document.body.classList.remove('modal-open');
+	  this.isModalVisible = false;
+  }
+
+  editAuction(): void {
+    
   }
 
   ngOnDestroy(): void {
