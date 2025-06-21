b24a86e9f2a712c4881becbfd2125b190612d1d8
M-Bell <maxymshyjko@gmail.com>
2025-06-21 16:17:35 +0100

    Configured location strategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,15 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideCommonServices } from './services/common/common.providers';
+import { provideHttpClient } from '@angular/common/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(),
+    {provide: LocationStrategy, useClass: HashLocationStrategy},
     provideClientHydration(),
     ...provideCommonServices()
   ]
