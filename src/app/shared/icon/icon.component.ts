644d4761252223f29971fe266fb8c74749541090
M-Bell <maxymshyjko@gmail.com>
2025-06-27 09:58:04 +0100

    Add icon color change on hover

--- src/app/shared/icon/icon.component.ts
+++ src/app/shared/icon/icon.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, DoCheck, HostListener, Input, OnChanges, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Color } from '../enums/color.enum';
 
@@ -10,12 +10,19 @@ import { Color } from '../enums/color.enum';
   templateUrl: './icon.component.html',
   styleUrl: './icon.component.scss'
 })
-export class IconComponent implements OnChanges {
+export class IconComponent implements OnInit{
   @Input() name!: string;
   @Input() size: number = 24;
   @Input() color: Color = Color.ACCENT_ORANGE;
+  @Input() hoverColor:Color = Color.ACCENT_ORANGE;
 
+  rawSvg: string = '';
   svgContent: any;
+  hovered = false;
+
+  get effectiveColor(): Color {
+    return this.hovered ? this.hoverColor : this.color;
+  }
 
   private colorMap: Record<Color, string> = {
     [Color.MAIN_GREEN]: '#253B2D',
@@ -26,19 +33,41 @@ export class IconComponent implements OnChanges {
 
   constructor(private http: HttpClient, private sanitizer: DomSanitizer) {}
 
-  ngOnChanges(): void {
-    if (this.name) {
-      this.http
-        .get(`./assets/icons/${this.name}.svg`, { responseType: 'text' })
-        .subscribe(data => {
-          const styledSvg = data
-            .replace('<svg', `<svg width="${this.size}" 
-                                   height="${this.size}" 
-                                   viewBox="0 0 ${this.size} ${this.size}" 
-                                   stroke="${this.colorMap[this.color]}"
-                                   fill="none"`);
-          this.svgContent = this.sanitizer.bypassSecurityTrustHtml(styledSvg);
-        });
-    }
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    if(this.hovered) return;
+    this.hovered = true;
+    this.renderSvg();
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    if(!this.hovered) return;
+    this.hovered = false;
+    this.renderSvg();
+  }
+
+  ngOnInit(): void {
+    this.loadSvg();
+  }
+
+  private loadSvg(): void {
+  this.http
+    .get(`assets/icons/${this.name}.svg`, { responseType: 'text' })
+    .subscribe(data => {
+      this.rawSvg = data;
+      this.renderSvg();
+    });
+}
+
+  private renderSvg(): void{
+    console.log(2)
+      const styledSvg = this.rawSvg
+        .replace('<svg', `<svg width="${this.size}" 
+                                height="${this.size}" 
+                                viewBox="0 0 ${this.size} ${this.size}" 
+                                stroke="${this.colorMap[this.effectiveColor]}"
+                                fill="none"`);
+      this.svgContent = this.sanitizer.bypassSecurityTrustHtml(styledSvg);
   }
 }
