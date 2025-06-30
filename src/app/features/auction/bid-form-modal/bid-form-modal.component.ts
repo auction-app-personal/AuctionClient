e69b15e377dc1c2293affbf5bdfc940648065936
M-Bell <maxymshyjko@gmail.com>
2025-06-30 09:59:39 +0100

    Created component for bid modal form

---
+++ src/app/features/auction/bid-form-modal/bid-form-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bid-form-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './bid-form-modal.component.html',
+  styleUrl: './bid-form-modal.component.scss'
+})
+export class BidFormModalComponent {
+
+}
