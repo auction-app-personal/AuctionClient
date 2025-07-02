35f94862a68555278f7aae45941e65c7da28a3bf
M-Bell <maxymshyjko@gmail.com>
2025-07-02 09:08:23 +0100

    Added lot modal form

--- src/app/features/lot/lot-form-modal/lot-form-modal.component.ts
+++ src/app/features/lot/lot-form-modal/lot-form-modal.component.ts
@@ -1,13 +1,61 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Inject, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { LotStatus } from '../../../models/lot/lot.model';
+import { LOT_SERVICE } from '../../../services/common/injection-tokens';
+import { LotService } from '../../../services/data/lot/lot-service.interface';
 
 @Component({
   selector: 'app-lot-form-modal',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './lot-form-modal.component.html',
   styleUrl: './lot-form-modal.component.scss'
 })
-export class LotFormModalComponent {
-@Input({required: true})
-auctionId!: number;
+export class LotFormModalComponent implements OnInit {
+
+  @Input({required: true}) auctionId!: number;
+  @Output() close = new EventEmitter<void>();
+  @Output() save = new EventEmitter<void>();
+
+
+  lotForm!: FormGroup;
+  lotName!: FormControl;
+  lotDescr!: FormControl;
+  lotStartPrice!: FormControl;
+
+  constructor(@Inject(LOT_SERVICE) private lotService: LotService){
+
+  }
+
+  ngOnInit(): void {
+    this.lotForm = new FormGroup({
+      name: new FormControl(this.lotName, [Validators.required]),
+      description: new FormControl(this.lotDescr),
+      startPrice: new FormControl(this.lotStartPrice),
+    });
+  }
+
+  saveLot(): void {
+    this.lotService.create({
+      id: 0,
+      name: this.lotForm.value["name"],
+      description: this.lotForm.value["description"],
+      auctionId: this.auctionId,
+      startPrice: this.lotForm.value["startPrice"],
+      status: LotStatus.AWAITING_AUCTION
+    }).subscribe(
+      (value) => {
+        if(value.id === 0){
+          console.log('fail')
+        } else {
+          this.save.emit()
+          this.close.emit();
+        }
+      }
+    );
+  }
+
+  closeModal(): void {
+    this.close.emit();
+  }
 }
