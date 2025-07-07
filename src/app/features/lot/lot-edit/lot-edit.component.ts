40fff0442cd38ef0a189b0ca5f116648e9e4b709
M-Bell <maxymshyjko@gmail.com>
2025-07-07 09:39:05 +0100

    Created lot edit window

--- src/app/features/lot/lot-edit/lot-edit.component.ts
+++ src/app/features/lot/lot-edit/lot-edit.component.ts
@@ -1,12 +1,96 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { LotFormModalComponent } from "../lot-form-modal/lot-form-modal.component";
+import { Color } from '../../../shared/enums/color.enum';
+import { LotDto } from '../../../models/lot/lot.model';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { LotService } from '../../../services/data/lot/lot-service.interface';
+import { AUCTION_FACADE, LOT_SERVICE } from '../../../services/common/injection-tokens';
+import { AuctionFacadeService } from '../../../services/shared/auction-facade.service';
+import { Location } from '@angular/common';
+import { IconComponent } from "../../../shared/icon/icon.component";
 
 @Component({
   selector: 'app-lot-edit',
   standalone: true,
-  imports: [],
+  imports: [LotFormModalComponent, IconComponent],
   templateUrl: './lot-edit.component.html',
   styleUrl: './lot-edit.component.scss'
 })
 export class LotEditComponent {
+  Color = Color;
 
+  lot: LotDto | null = null;
+
+  paricipants: number = 0;
+  totalCollected: number = 0;
+  timeRemaining: Date = new Date();
+
+  private subscriptions: Subscription = new Subscription();
+  isModalVisible: boolean = false;
+
+  constructor(
+    @Inject(LOT_SERVICE) private lotService: LotService,
+    @Inject(AUCTION_FACADE) private auctionFacade: AuctionFacadeService,
+    private route: ActivatedRoute,
+    private location: Location
+  )  {
+    
+  }
+  ngOnInit(): void {
+    const routeSubscription = this.route.paramMap.subscribe((params) => {
+      const idVal = params.get('id');
+      if (idVal) {
+        this.lotService.getById(parseInt(idVal)).subscribe(
+          lot => {
+            this.lot = lot;
+          }
+        )
+      }
+    });
+    this.subscriptions.add(routeSubscription);
+  }
+
+  reloadLot() {
+    if(!this.lot) return;
+    
+    this.lotService.getById(this.lot?.id).subscribe(
+      lot => {
+        this.lot = lot;
+      }
+    )
+  }
+
+  hideLotModal() {
+    document.body.classList.remove('modal-open');
+    this.isModalVisible = false;
+  }
+
+  openLotModal() {
+    document.body.classList.add('modal-open');
+    this.isModalVisible = true;
+  }
+
+  // changelotStatus(status: lotStatus){
+  //   if(!this.lot) return;
+  //   this.lot.status = status;
+  //   this.lotService.update(this.lot.id, this.lot).subscribe(
+  //     (newVal) => this.lot = newVal
+  //   )
+  // }
+
+  deleteLot() {
+    if(!this.lot) return;
+    this.lotService.delete(this.lot.id).subscribe(
+      {
+      complete: () => {
+        this.location.back();
+      }
+    }
+    )
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 }
--- src/app/features/lot/lot-form-modal/lot-form-modal.component.ts
+++ src/app/features/lot/lot-form-modal/lot-form-modal.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Inject, Input, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { LotStatus } from '../../../models/lot/lot.model';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { LotDto, LotStatus } from '../../../models/lot/lot.model';
 import { LOT_SERVICE } from '../../../services/common/injection-tokens';
 import { LotService } from '../../../services/data/lot/lot-service.interface';
 
@@ -14,43 +14,42 @@ import { LotService } from '../../../services/data/lot/lot-service.interface';
 export class LotFormModalComponent implements OnInit {
 
   @Input({required: true}) auctionId!: number;
+  @Input({required: false}) lot: LotDto | null = null;
   @Output() close = new EventEmitter<void>();
   @Output() save = new EventEmitter<void>();
 
 
   lotForm!: FormGroup;
-  lotName!: FormControl;
-  lotDescr!: FormControl;
-  lotStartPrice!: FormControl;
 
-  constructor(@Inject(LOT_SERVICE) private lotService: LotService){
+  constructor(@Inject(LOT_SERVICE) private lotService: LotService,
+              private fb: FormBuilder){
 
   }
 
   ngOnInit(): void {
-    this.lotForm = new FormGroup({
-      name: new FormControl(this.lotName, [Validators.required]),
-      description: new FormControl(this.lotDescr),
-      startPrice: new FormControl(this.lotStartPrice),
+    this.lotForm = this.fb.group({
+      name: [this.lot?.name ?? '', Validators.required],
+      description: [this.lot?.description ?? ''],
+      startPrice: [this.lot?.startPrice ?? 0],
     });
   }
 
   saveLot(): void {
-    this.lotService.create({
-      id: 0,
-      name: this.lotForm.value["name"],
-      description: this.lotForm.value["description"],
-      auctionId: this.auctionId,
-      startPrice: this.lotForm.value["startPrice"],
-      status: LotStatus.AWAITING_AUCTION
+    this.lotService.save({
+      id: this.lot?.id ?? 0,
+      name: this.lotForm.get("name")?.value,
+      description: this.lotForm.get("description")?.value,
+      auctionId: this.lot?.auctionId ?? this.auctionId,
+      startPrice: this.lotForm.get("startPrice")?.value,
+      status: this.lot?.status ?? LotStatus.AWAITING_AUCTION
     }).subscribe(
       (value) => {
         if(value.id === 0){
           console.log('fail')
-        } else {
-          this.save.emit()
-          this.close.emit();
+          return;
         }
+        this.save.emit();
+        this.close.emit();
       }
     );
   }
