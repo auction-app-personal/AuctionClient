750a04010c3ec41073b390d59b844ca4bccc98cf
M-Bell <maxymshyjko@gmail.com>
2025-07-29 09:23:36 +0100

    Added simple bid modal layout

--- src/app/features/auction/bid-form-modal/bid-form-modal.component.ts
+++ src/app/features/auction/bid-form-modal/bid-form-modal.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Inject, Input, Output } from '@angular/core';
+import { BidDto } from '../../../models/bid/bid.model';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AUCTION_FACADE } from '../../../services/common/injection-tokens';
+import { AuctionFacadeService } from '../../../services/shared/auction-facade.service';
 
 @Component({
   selector: 'app-bid-form-modal',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './bid-form-modal.component.html',
   styleUrl: './bid-form-modal.component.scss'
 })
 export class BidFormModalComponent {
+  @Input({required: true}) lotId!: number;
 
+  @Output() close = new EventEmitter<void>();
+  @Output() save = new EventEmitter<void>();
+
+
+  bidForm!: FormGroup;
+
+  constructor(@Inject(AUCTION_FACADE) private auctionFacade: AuctionFacadeService,
+              private fb: FormBuilder){
+
+  }
+
+  ngOnInit(): void {
+    this.bidForm = this.fb.group({
+      amount: [0, Validators.required],
+    });
+  }
+
+  placeBid(): void {
+    // this.auctionFacade.placeBid().subscribe(
+    //   (value) => {
+    //     if(value.id === 0){
+    //       console.log('fail')
+    //       return;
+    //     }
+    //     this.save.emit();
+    //     this.close.emit();
+    //   }
+    // );
+  }
+
+  closeModal(): void {
+    this.close.emit();
+  }
 }
--- src/app/features/auction/lot-table/lot-table.component.ts
+++ src/app/features/auction/lot-table/lot-table.component.ts
@@ -1,15 +1,36 @@
 import { Component, Input } from '@angular/core';
 import { LotDto } from '../../../models/lot/lot.model';
 import { LotItemExpandableComponent } from "./lot-item-expandable/lot-item-expandable.component";
+import { BidFormModalComponent } from "./../bid-form-modal/bid-form-modal.component";
 
 @Component({
   selector: 'app-lot-table',
   standalone: true,
-  imports: [LotItemExpandableComponent],
+  imports: [
+    LotItemExpandableComponent,
+    BidFormModalComponent
+  ],
   templateUrl: './lot-table.component.html',
   styleUrl: './lot-table.component.scss',
 })
 export class LotTableComponent {
   @Input({ required: true })
   lots!: LotDto[] | null;
+  isModalVisible: boolean = false;
+  chosenLotId: number = 0;
+
+  hideBidModal() {
+    document.body.classList.remove('modal-open');
+    this.isModalVisible = false;
+  }
+    
+  openBidModal(lotId: number) {
+    document.body.classList.add('modal-open');
+    this.chosenLotId = lotId;
+    this.isModalVisible = true;
+  }
+
+  reloadView() {
+    throw new Error('Method not implemented.');
+  }
 }
