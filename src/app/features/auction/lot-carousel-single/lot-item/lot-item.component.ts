56c619672098b132ac1a077965e974041953db3d
M-Bell <maxymshyjko@gmail.com>
2025-09-30 21:34:44 +0100

    Added bid placement in lot carousel

--- src/app/features/auction/lot-carousel-single/lot-item/lot-item.component.ts
+++ src/app/features/auction/lot-carousel-single/lot-item/lot-item.component.ts
@@ -6,21 +6,26 @@ import { BidDto } from '../../../../models/bid/bid.model';
 import { AUCTION_FACADE } from '../../../../services/common/injection-tokens';
 import { AuctionFacadeService } from '../../../../services/shared/auction-facade.service';
 import { AsyncPipe } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../../../services/auth/auth.service';
 
 @Component({
   selector: 'app-lot-item',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, ReactiveFormsModule],
   templateUrl: './lot-item.component.html',
   styleUrl: './lot-item.component.scss',
 })
 export class LotItemComponent implements OnInit, OnChanges{
   @Input({ required: true })
   lot!: LotDto;
+  bidForm!: FormGroup;
 
   highestBidInfo$!: Observable<{account: AccountDto| null, bid: BidDto | null }>;
 
-  constructor(@Inject(AUCTION_FACADE) private facade: AuctionFacadeService){
+  constructor(@Inject(AUCTION_FACADE) private facade: AuctionFacadeService,
+              private authService: AuthService,
+              private fb: FormBuilder){
 
   }
   ngOnChanges(): void {
@@ -28,6 +33,25 @@ export class LotItemComponent implements OnInit, OnChanges{
   }
 
   ngOnInit(): void {
+    this.bidForm = this.fb.group({
+      amount: [0, Validators.required],
+    });
     this.highestBidInfo$ = this.facade.highestBidInfoForLot$(this.lot!.id);
   }
+
+  
+  placeBid(): void {
+    
+    this.facade.placeBid(this.authService.currentUser()?.id ?? 0, this.lot.id, this.bidForm.get("amount")?.value).subscribe(
+      (value) => {
+        if(value.id === 0){
+          console.log('fail')
+          return;
+        } else {
+          this.bidForm.reset();
+        }
+      }
+    );
+  }
+  
 }
